5f48a518f8ebb94a79a7d6c49e3c64afbb101182
manny <immanuelmonroe@gmail.com>
2023-05-17 20:17:32 -0400

    Added feature branc & initial steps in JS

---
+++ script.js
@@ -0,0 +1,10 @@
+// You’re going to store the gameboard as an array inside of a Gameboard object
+
+
+// Your players are also going to be stored in objects
+
+// you’re probably going to want an object to control the flow of the game itself.
+
+
+// Your main goal here is to have as little global code as possible. Try tucking everything away inside of a module or factory.
+// Rule of thumb: if you only ever need ONE of something (gameBoard, displayController), use a module.If you need multiples of something (players!), create them with factories.
