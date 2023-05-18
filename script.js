7086592402ef39bdd2893ccc54a168a9b5c2d611
manny <immanuelmonroe@gmail.com>
2023-05-17 20:25:40 -0400

    Created first iteration of gameboard object

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
 // You’re going to store the gameboard as an array inside of a Gameboard object
-
+function Gameboard() {
+    // Initialize the game board as an empty array
+    this.board = [];
+  
+    // Method to initialize the game board with empty cells
+    this.initializeBoard = function(size) {
+      this.board = Array(size).fill(null).map(() => Array(size).fill(null));
+    };
+  
+    // Method to retrieve the value of a specific cell on the game board
+    this.getCell = function(row, col) {
+      return this.board[row][col];
+    };
+  
+    // Method to set the value of a specific cell on the game board
+    this.setCell = function(row, col, value) {
+      this.board[row][col] = value;
+    };
+  
+    // Method to print the current game board
+    this.printBoard = function() {
+      console.log(this.board);
+    };
+  }
+  
+  // Usage
+  const gameboard = new Gameboard();
+  gameboard.initializeBoard(3);
+  gameboard.setCell(0, 0, 'X');
+  gameboard.setCell(1, 1, 'O');
+  gameboard.printBoard();
 
 // Your players are also going to be stored in objects
 
