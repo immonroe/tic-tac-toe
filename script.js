d12ab46fea43180a1b95ed928b778d4ee127ef7a
manny <immanuelmonroe@gmail.com>
2023-05-22 20:29:06 -0400

    Added proof of concept code/adjusted CSS

--- script.js
+++ script.js
@@ -42,12 +42,12 @@ const Player = (name, symbol) => {
   };
 
 // Create a gameboard and initialize it
-Gameboard.initializeBoard(3); //using 3x3 rows for tic-tac-toe board
+// Gameboard.initializeBoard(3); //using 3x3 rows for tic-tac-toe board
 
 // Set cells on the game board - examples below
-Gameboard.setCell(0, 0, 'X');
-Gameboard.setCell(1, 1, 'O');
-Gameboard.setCell(2, 2, 'X');
+// Gameboard.setCell(0, 0, 'X');
+// Gameboard.setCell(1, 1, 'O');
+// Gameboard.setCell(2, 2, 'X');
 
 // Print the game board
 Gameboard.printBoard();
@@ -59,3 +59,60 @@ const player2 = Player('Player 2', 'O');
 // Make moves
 const move1 = player1.makeMove();
 const move2 = player2.makeMove();
+
+
+// proof of concept
+
+let playerSymbol = '';
+let aiSymbol = '';
+let cells = document.querySelectorAll(".cell")
+
+cells.forEach(function(node){
+  node.addEventListener('click', function(){
+    if (playerSymbol == '') {
+      playerSymbol = window.prompt('Would you like to be X or O?', 'X')
+    }
+
+    if (playerSymbol !== 'X' && playerSymbol !== 'O') {
+      alert(`${playerSymbol} is not valid`)
+      playerSymbol = ''
+      return 1; 
+    }
+
+    aiSymbol = playerSymbol === 'X' ? 'O' : 'X'
+    node.innerText = playerSymbol;
+
+    let availableCellCount = 9;
+    let aiPick = 0;
+    let isValidAiPick = false;
+
+    do {
+      availableCellCount = Array.from(cells).filter(cell => cell.innerText == '').length
+
+      aiPick = Math.floor(Math.random() * 9)
+      isValidAiPick = cells[aiPick].innerText === '';
+    }
+    while (!isValidAiPick && availableCellCount > 1);
+
+    console.log(aiPick)
+
+    // create a function call to check symbols too see if player or CPU won - pass in a string inside node to see values
+    // 
+    // cells[0].innerText has won!
+
+    if (cells[0].innerText === cells[1].innerText && cells[1].innerText === cells[2].innerText) console.log('winner')
+    if (cells[3].innerText === cells[4].innerText && cells[4].innerText === cells[5].innerText) console.log('winner')
+    if (cells[6].innerText === cells[7].innerText && cells[7].innerText === cells[8].innerText) console.log('winner')
+    
+    if (cells[0].innerText === cells[3].innerText && cells[3].innerText === cells[6].innerText) console.log('winner')
+    if (cells[1].innerText === cells[4].innerText && cells[4].innerText === cells[7].innerText) console.log('winner')
+    if (cells[2].innerText === cells[5].innerText && cells[5].innerText === cells[8].innerText) console.log('winner')
+    
+    if (cells[0].innerText === cells[1].innerText && cells[1].innerText === cells[2].innerText) console.log('winner')
+    if (cells[0].innerText === cells[1].innerText && cells[1].innerText === cells[2].innerText) console.log('winner')
+
+    if (availableCellCount > 1) {
+      cells[aiPick].innerText = aiSymbol;
+    }
+  })
+})
